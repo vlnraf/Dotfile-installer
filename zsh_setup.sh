204ff2402f2b871d9d7b003688563799c4669035
vlnraf <raffaele.villani17@gmail.com>
2021-10-11 22:22:36 +0200

    added zsh-autosuggestions install

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -1,6 +1,7 @@
 install_oh_my_zsh(){
     sudo pacman -S zsh
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 }
 
 install_oh_my_zsh
